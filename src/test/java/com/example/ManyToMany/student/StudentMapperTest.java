2ff8b8749b2d03c7d45b8981ecea99066236bd9c
Steven <stevenmb1995@gmail.com>
2025-01-16 20:21:53 -0600

    added more tests for student mapper

--- src/test/java/com/example/ManyToMany/student/StudentMapperTest.java
+++ src/test/java/com/example/ManyToMany/student/StudentMapperTest.java
@@ -21,6 +21,36 @@ class StudentMapperTest {
     @Test
     void toStudent() {
         LocalDateTime now=LocalDateTime.now();
+        StudentDto dtoUpdate=new StudentDto(
+                123,
+                "John Doe",
+                "john@example.com",
+                "123",
+                "senior",
+                false,
+                new HashSet<>());
+        Student student= new Student(
+                123,
+                "John",
+                "test@test.com",
+                "123",
+                "senior",
+                false,
+                new HashSet<>(),
+                now
+        );
+        Student result=mapper.toStudent(dtoUpdate);
+        Assertions.assertNotNull(result.getId());
+        Assertions.assertEquals(student.getName(),result.getName());
+        Assertions.assertEquals(student.getEmail(),result.getEmail());
+        Assertions.assertEquals(student.getPassword(),result.getPassword());
+        Assertions.assertEquals(student.getRole(),result.getRole());
+        Assertions.assertEquals(student.isAcademicProbation(),result.isAcademicProbation());
+    }
+
+    @Test
+    void toStudentNullId() {
+        LocalDateTime now=LocalDateTime.now();
         StudentCreateDto dtoCreate=new StudentCreateDto(
                 "John Doe",
                 "john@example.com",
@@ -41,13 +71,40 @@ class StudentMapperTest {
         Student result=mapper.toStudentNullId(dtoCreate);
         Assertions.assertNull(result.getId());
         Assertions.assertEquals(student.getName(),result.getName());
-    }
-
-    @Test
-    void toStudentNullId() {
+        Assertions.assertEquals(student.getEmail(),result.getEmail());
+        Assertions.assertEquals(student.getPassword(),result.getPassword());
+        Assertions.assertEquals(student.getRole(),result.getRole());
+        Assertions.assertEquals(student.isAcademicProbation(),result.isAcademicProbation());
     }
 
     @Test
     void toStudentResponseDto() {
-    }
+        LocalDateTime now=LocalDateTime.now();
+        Student student= new Student(
+                123,
+                "John",
+                "test@test.com",
+                "123",
+                "senior",
+                false,
+                new HashSet<>(),
+                now
+        );
+        StudentResponseDto studentResponseDto=new StudentResponseDto(
+                123,
+                "John Doe",
+                "john@example.com",
+                "123",
+                "senior",
+                false,
+                new HashSet<>());
+
+        StudentResponseDto result=mapper.toStudentResponseDto(student);
+        Assertions.assertNotNull(result.id());
+        Assertions.assertEquals(studentResponseDto.name(),result.name());
+        Assertions.assertEquals(studentResponseDto.email(),result.email());
+        Assertions.assertEquals(studentResponseDto.password(),result.password());
+        Assertions.assertEquals(studentResponseDto.role(),result.role());
+        Assertions.assertEquals(studentResponseDto.academicProbation(),result.academicProbation());
+    };
 }
